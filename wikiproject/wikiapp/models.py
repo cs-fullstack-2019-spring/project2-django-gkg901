361ad7e8d5f9183e7f0c74f9120dcfb6b8b39818
gkg901 <gerren.gregory@gmail.com>
2019-03-18 07:42:15 -0500

    update

--- wikiproject/wikiapp/models.py
+++ wikiproject/wikiapp/models.py
@@ -19,7 +19,7 @@ class PostModel(models.Model):
 class RelatedModel(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField(max_length=300)
-    image = models.ImageField(null=True, blank=True, upload_to='media')
+    image = models.ImageField(null=True, blank=True, upload_to='wiki')
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(default=timezone.now)
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, null=True, blank=True)
